2c31cbcd431435ddbd3ee834f7640bc0088b3b32
Przemek <pk.korzen1@gmail.com>
2020-05-13 20:39:36 +0200

    modified Player entity

--- src/main/java/com/pkkor/pandemic/entities/player/Player.java
+++ src/main/java/com/pkkor/pandemic/entities/player/Player.java
@@ -1,34 +1,59 @@
 package com.pkkor.pandemic.entities.player;
 
-import com.pkkor.pandemic.actions.*;
 import com.pkkor.pandemic.enums.cards.Card;
-import com.pkkor.pandemic.enums.cards.CityCards;
 import com.pkkor.pandemic.enums.characters.Characters;
 
 public class Player {
+    private int Id;
     private Characters character;
-    private Treatable treatmentAction;
-    private Shareable sharingAction;
-    private Movable movingAction;
-    private Cureable curingAction;
-    private Buildable buildingAction;
-    private CityCards city;
+    private String city;
     private int actionsNumber;
     private Card[] cards;
 
-    public Player(Characters character){
+    public Player(Characters character) {
         this.character = character;
-        this.treatmentAction = character.getDiseaseTreatmentAction();
-        this.sharingAction = character.getKnowledgeSharingAction();
-        this.movingAction = character.getMovingAction();
-        this.curingAction = character.getCuringAction();
-        this.buildingAction = character.getBuildingAction();
-        this.city = CityCards.ATLANTA;
+        this.city = "Atlanta";
         this.actionsNumber = 4;
         this.cards = new Card[7];
     }
 
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public Characters getCharacter() {
+        return character;
+    }
+
+    public void setCharacter(Characters character) {
+        this.character = character;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getActionsNumber() {
+        return actionsNumber;
+    }
+
+    public void setActionsNumber(int actionsNumber) {
+        this.actionsNumber = actionsNumber;
+    }
+
     public Card[] getCards() {
         return cards;
+    }
+
+    public void setCards(Card[] cards) {
+        this.cards = cards;
     }
 }
