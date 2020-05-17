ea20b2cb22b93b7ffa32a54246d5da15aac92539
Przemek <pk.korzen1@gmail.com>
2020-05-17 11:41:47 +0200

    modified PlayerController to use PlayerDTO

--- src/main/java/com/pkkor/pandemic/controllers/PlayerController.java
+++ src/main/java/com/pkkor/pandemic/controllers/PlayerController.java
@@ -1,30 +1,37 @@
 package com.pkkor.pandemic.controllers;
 
-import com.pkkor.pandemic.entities.player.Player;
+import com.pkkor.pandemic.dto.PlayerDTO;
+import com.pkkor.pandemic.mappers.PlayerMapper;
 import com.pkkor.pandemic.services.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class PlayerController {
 
     private final PlayerService playerService;
+    private final PlayerMapper playerMapper;
 
     @Autowired
-    public PlayerController(PlayerService playerService) {
+    public PlayerController(PlayerService playerService, PlayerMapper playerMapper) {
         this.playerService = playerService;
+        this.playerMapper = playerMapper;
     }
 
     @GetMapping("/players")
-    public List<Player> findAll() {
-        return playerService.findAllPlayers();
+    public List<PlayerDTO> findAll() {
+        return playerService.findAllPlayers()
+                .stream()
+                .map(playerMapper::convert)
+                .collect(Collectors.toList());
     }
 
-    @PostMapping("/players")
-    void savePlayers(@RequestBody Player player) {
-        playerService.savePlayer(player);
+    @PostMapping("/player/save")
+    void savePlayers(@RequestBody PlayerDTO playerDTO) {
+        playerService.savePlayer(playerMapper.convert(playerDTO));
     }
 }
